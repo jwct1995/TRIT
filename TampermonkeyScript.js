d5a343f5daac0886cd2d5825880b0b6b6885e7b9
Jason WCT <jasonwong_1995@hotmail.com>
2023-05-13 01:16:41 +0800

    x

--- TampermonkeyScript.js
+++ TampermonkeyScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TRIT
 // @namespace    http://tampermonkey.net/
-// @version      18.2
+// @version      18.5
 // @description  make life easy
 // @author       JWCT
 // @match        http://34.87.111.75/*
